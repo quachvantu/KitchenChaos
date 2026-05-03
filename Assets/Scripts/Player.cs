4b40cfdc44709397fee58db0b54b32a17e396fab
quachvantu <quachvantu@gmail.com>
2026-05-03 18:25:50 +0700

    feat : add wall sliding when movement is blocked

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -20,6 +20,27 @@ public class Player : MonoBehaviour
         float playerRadius = .7f;
         float playerHeight = 2f;
         bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+        if (!canMove)
+        {
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            if (canMove)
+            {
+                moveDir = moveDirX;
+            }
+            else
+            {
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+                }
+            }
+        }
         if (canMove)
         {
             transform.position += moveDir * moveDistance;
